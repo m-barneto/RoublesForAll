5262ca9fb075d067577d41095a37579590961f97
m-barneto <4347791+m-barneto@users.noreply.github.com>
2024-02-25 18:07:19 -0500

    Renamed mod class, added LICENSE to .buildignore

--- src/mod.ts
+++ src/mod.ts
@@ -5,13 +5,11 @@ import { ILogger } from "@spt-aki/models/spt/utils/ILogger";
 import { DatabaseServer } from "@spt-aki/servers/DatabaseServer";
 import { LogTextColor } from "@spt-aki/models/spt/logging/LogTextColor";
 
-class Mod implements IPostDBLoadMod
+class RoublesForAll implements IPostDBLoadMod
 {
     private modConfig = require("../config/config.json");
 
     public postDBLoad(container: DependencyContainer): void {
-        // Database will be loaded, this is the fresh state of the DB so NOTHING from the AKI
-        // logic has modified anything yet. This is the DB loaded straight from the JSON files
         const databaseServer = container.resolve<DatabaseServer>("DatabaseServer");
         const logger = container.resolve<ILogger>("WinstonLogger");
 
